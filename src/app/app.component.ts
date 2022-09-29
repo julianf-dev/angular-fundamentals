fb51b7342e3da131c7374747940ee90f4e33f746
JulianF <juliandavidbf@gmail.com>
2022-09-29 14:06:38 -0400

    @init: Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-first-project';
+
+  title:string = 'Hello World';
+  name:string = 'Julian';
+  age: number = 27;
+  img: string = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Julian',
+    age: 28,
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age++;
+  }
+
+  onScroll(event: Event){
+    const element = event.target  as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event){
+    const element = event.target  as HTMLInputElement;
+    element.value;
+
+    this.person.name = element.value;
+
+  }
 }
