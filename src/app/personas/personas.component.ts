c44854d0ab73b54a080840183f2818f8044f8b96
JulianF <juliandavidbf@gmail.com>
2022-11-25 16:48:07 -0500

    @feat: practice

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,8 +8,19 @@ import { Component } from "@angular/core";
 export class PersonasComponent {
   disabled = false;
   mensaje = 'Empty person'
+  title: string = ''
+  showPerson : boolean = false;
 
-  getPerson(){
+  addPerson(){
+    this.title === '' ?
+    this.showPerson === false :
+    this.showPerson = true
     this.mensaje = 'Person add sucessly'
+  }
+
+  changeTitle(event: Event){
+    this.showPerson = true ? this.showPerson = false : this.showPerson = true;
+    this.title = (<HTMLInputElement>event.target).value;
+    console.log('Metodo change title')
   }
 }
